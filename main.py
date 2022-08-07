96f3638054a40ebefc5e51368cbef3abade88074
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2022-08-07 17:35:35 +0300

    feat: implement core logic

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ port = 3000
 hostname = '127.0.0.1'
 
 s.bind((hostname, port))
+
+print('Listening at {}'.format(s.getsockname()))
+
+
+MAX_SIZE_BYTES = 65535 # Mazimum size of a UDP datagram
+
+while True:
+    data, clientAddress = s.recvfrom(MAX_SIZE_BYTES)# Receive at most 65535 bytes at once
+    message = data.decode('ascii')
+    uppercase_msg = message.upper()
+    print('The client at {} says {!r}'.format(clientAddress, message))
+    data = uppercase_msg.encode('ascii')
+    s.sendto(data, clientAddress)
